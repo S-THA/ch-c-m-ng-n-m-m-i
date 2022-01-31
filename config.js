0e6de42fe90dc70b039f8c20ef4ad8925f2ad809
S-THA <95969180+S-THA@users.noreply.github.com>
2022-01-31 17:14:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const CONFIG = {
     introTitle: 'NGA À!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao Nga cứ Xinh đẹp như dị !!!
-    CHÚC MỪNG NĂM MỚI`,
+         CHÚC MỪNG NĂM MỚI`,
     btnIntro: 'NHẤN DÔ NÈ HIHI',
-    title: ' NGA ĐẾN TỪ CUNG CHĂNG ĐÚNG HEM 😙',
-    desc: 'NGA ĐỂ CHO THA ƯỚC MỘT ĐIỀU ƯỚC ĐƯỢC KHÔNG 😘😘 ',
+    title:'NGA dến từ cung trăng đúng hem 😙',
+    desc: 'NGA để tha ước một điều được ko 😘😘 ',
     btnYes: 'CHẤP NHẬN NÈ (BẤT KÌ ĐIỀU CHI)',
     btnNo: 'Không, ĐỂ SUY NGHĨ XÍU NHO  ',
     question:'TRÊN TRỜI CÓ VẠN VÌ SAO CHÚC NGA NĂM MỚI DUI DẼ NÈ ,XINH ĐẸP CUTE HƯN NÈ ',
